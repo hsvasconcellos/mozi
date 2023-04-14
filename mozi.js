57492e7b4dd0de683694fd460c6c3a854bbcad20
hsvasconcellos <61476196+hsvasconcellos@users.noreply.github.com>
2023-04-13 23:50:10 -0300

    update3

--- mozi.js
+++ mozi.js
@@ -1,5 +1,6 @@
 var botao = document.querySelector(".conteudo");
 var caixa = document.querySelector(".box");
+var fundo = document.querySelector(".container");
 
 function esconderBotao() {
   botao.classList.toggle("hide");
@@ -9,8 +10,14 @@ function adicionarImg() {
   caixa.style.backgroundImage = "url('ananda.jpg')";
 }
 
+function adicionarFundo() {
+  fundo.style.backgroundImage = "url('coracao.jpg')";
+}
+
 botao.addEventListener("click", () => {
   esconderBotao();
 
   adicionarImg();
+
+  adicionarFundo();
 });
